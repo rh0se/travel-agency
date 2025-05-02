4e39da93d54ba83c09fa53253ed0223e61605ade
Rhoda Ojetola <momirhoda@gmail.com>
2025-05-02 16:21:38 +0100

    implemented desktop sidebar

--- app/routes/admin/admin-layout.tsx
+++ app/routes/admin/admin-layout.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import { Outlet } from 'react-router'
+import { SidebarComponent } from "@syncfusion/ej2-react-navigations"
+import { NavItems } from 'components'
 
 const AdminLayout = () => {
     return (
         <div className='admin-layout'>
             ADMIN LAYOUT
+            <aside className='w-full max-w-[270px] hidden lg:block'>
+                <SidebarComponent width={270} enableGestures={false}>
+                    <NavItems />
+                </SidebarComponent>
+            </aside>
             <Outlet />
         </div>
     )
