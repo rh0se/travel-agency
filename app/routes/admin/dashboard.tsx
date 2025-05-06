f79a38d660032f184d0d7c54a57a0d07f98cbfae
Rhoda Ojetola <momirhoda@gmail.com>
2025-05-06 11:47:03 +0100

    updating stats cards

--- app/routes/admin/dashboard.tsx
+++ app/routes/admin/dashboard.tsx
@@ -1,12 +1,30 @@
-import { Header } from 'components'
+import { Header, StatsCard, TripCard } from 'components'
 
 const dashboard = () => {
     const user = { name: "Rhoda" }
+    const dashboardStats = {
+        totalUsers: 12450,
+        usersJoined: { currentMonth: 218, lastMonth: 176 },
+        totalTrips: 3210,
+        tripsCreated: { currentMonth: 150, lastMonth: 250 },
+        userRole: { total: 62, currentMonth: 25, lastMonth: 15 }
+    }
+
+    const { totalUsers, usersJoined, totalTrips, tripsCreated, userRole } = dashboardStats;
     return (
         <main className='dashboard wrapper'>
             <Header title={`Welcome ${user?.name ?? 'Guest'}`} description="Track activity, trends and popular destinations in real time" />
 
-            DASHBOARD
+            <section className='flex flex-col gap-6'>
+                <div className='grid grid-cols-1 md:grid-cols-3 gap-6 w-full'>
+                    <StatsCard headerTitle="Total Users" total={totalUsers} currentMonthCount={usersJoined.currentMonth} lastMonthCount={usersJoined.lastMonth} />
+                    <StatsCard headerTitle="Total Trips" total={totalTrips} currentMonthCount={tripsCreated.currentMonth} lastMonthCount={tripsCreated.lastMonth} />
+                    <StatsCard headerTitle="Active Users Today" total={userRole.total} currentMonthCount={userRole.currentMonth} lastMonthCount={userRole.lastMonth} />
+                </div>
+            </section>
+            <TripCard />
+
+
         </main>
     )
 }
